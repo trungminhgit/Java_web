843d862c5f869d9c0cb69d8cc73ab02d2202fcb4
Trung Minh <dangtrungminh456@gmail.com>
2023-07-09 14:36:29 +0700

    creat project

---
+++ SaleApp/src/main/java/com/dtm/controllers/IndexController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dtm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author ACER
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("msg","TEST");
+        return "index";
+    }
+}
---
+++ SaleApp/src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : index
+    Created on : Jul 9, 2023, 2:04:35 PM
+    Author     : ACER
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Trang chủ</title>
+    </head>
+    
+        <h1>Hello World!</h1>
+        <h2>${msg}</h2>
+    
+</html>
